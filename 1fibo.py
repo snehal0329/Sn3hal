0413152fcc393bac732d5dd191bef7ddd53cec17
snehal0329 <121428989+snehal0329@users.noreply.github.com>
2024-11-06 22:53:38 +0530

    Add files via upload

---
+++ 1fibo.py
@@ -0,0 +1,25 @@
+def recursive_fibonacci(n):
+    if n<=1:
+        return n
+    else:
+        return recursive_fibonacci(n-1) + recursive_fibonacci(n-2)
+
+def non_recursive_fibonacci(n):
+    first=0
+    second=1
+    print(first)
+    print(second)
+    while n-2>0:
+        third = first + second
+        first=second
+        second=third
+        print(third)
+        n-=1
+
+#if __name__=="__main__":
+n=5
+print("Recursive fibonacci")
+for i in range(n):
+    print(recursive_fibonacci(i))
+print(" Non Recursive fibonacci")
+non_recursive_fibonacci(n)
